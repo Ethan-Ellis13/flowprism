afe11d97a456f17bb8c8e66b4184c57aecb2ec18
Ethan-Ellis13 <ethan.d.ellis13@gmail.com>
2020-12-12 05:18:29 +0000

    Sticky Navbar

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Contact Form
 filterSelection("all");
 function filterSelection(c) {
   var x, i;
@@ -44,4 +45,22 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+
+// Sticky Navbar: When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
