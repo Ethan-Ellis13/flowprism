14573db12df3a5487a981018f5e5b26fa463492a
Ethan-Ellis13 <ethan.d.ellis13@gmail.com>
2020-12-12 04:48:35 +0000

    style

--- theme-switcher.js
+++ theme-switcher.js
@@ -5,13 +5,12 @@ const options = {
   time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorDark: '#ffffff',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true // default: true
 }
-
 const darkmode = new Darkmode(options);
 
 darkmode.showWidget();
